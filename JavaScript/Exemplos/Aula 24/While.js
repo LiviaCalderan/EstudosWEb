c8f6dc1608b96583bbfd3da1637a84c52bbfd95e
lívia <liv_calderan@hotmail.com>
2023-05-08 13:16:40 -0400

    aula 24

---
+++ JavaScript/Exemplos/Aula 24/While.js
@@ -0,0 +1,15 @@
+let velocidade = 80
+
+
+//Quando a velocidade ficar menor que zero, a condição se torna falsa, e ele para
+while (velocidade > 0) {
+    alert("O carro está a " + velocidade + " km/h")
+    velocidade -= 20
+    alert("Dominuindo 20 km/h")
+
+    if(velocidade === 40){
+        break
+    }
+}
+
+alert("O carro parou")
