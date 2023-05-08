da3019effdea45158a576ca767123a34eb0de103
lívia <liv_calderan@hotmail.com>
2023-05-08 11:03:07 -0400

    CorreçãoExercicio

--- JavaScript/Exercicios/Calculo de Dano/CalculoDano.js
+++ JavaScript/Exercicios/Calculo de Dano/CalculoDano.js
@@ -13,36 +13,30 @@
     do personagem defensor e exibir na tela a quantidade de dano e as informações atualizadas de ambos os personagens.
     */
 
-const attackingCharacter = prompt("Qual o nome do atacante?")
+const attackingCharacter = prompt("Qual o nome do personagem atacante?")
 const attackPower = parseFloat (prompt("Qual o poder de ataque do personagem?"))
 
-const defenseCharacter = prompt("Qual o nome do adversário?")
-const life = parseFloat (prompt("Informe a quantidade de pontos de vida: "))
+const defenseCharacter = prompt("Qual o nome do adversário defensor?")
+let life = parseFloat (prompt("Informe a quantidade de pontos de vida: "))
 const defencePower = parseFloat (prompt("Qual o poder de defesa do personagem?"))
 const shield = prompt("O personagem possui escudo? (sim/nao)")
-let danoSofrido
 
-if ( attackPower > defencePower ){
-    if ( shield == sim){
-            danoSofrido == (attackPower - defencePower) / 2
-            alert(
-                "Atualizações do personagem " + defenseCharacter +
-                "\n\nDano Sofrido: " + danoSofrido +
-                "\nPontos de vida restantes: " + (life - danoSofrido)
-            )
-    } else {
-        danoSofrido == (attackPower - defencePower)
-        alert(
-            "Atualizações do personagem " + defenseCharacter +
-            "\n\nDano Sofrido: " + danoSofrido +
-            "\nPontos de vida restantes: " + (life - danoSofrido)
-        )    
-    }
-} else {
-    let danoSofrido = 0
-    alert(
-        "Atualizações do personagem " + defenseCharacter +
-        "\n\nDano Sofrido: " + danoSofrido +
-        "\nPontos de vida restantes: " + (life - danoSofrido) 
-    )
+let danoCausado = 0
+
+if ( attackPower > defencePower && shield === "nao" ){
+    danoCausado = attackPower - defencePower
+} else if (attackPower > defencePower && shield === "sim"){
+    danoCausado = (attackPower - defencePower) / 2
 }
+
+alert (attackingCharacter + " causou " + danoCausado + " pontos de dano em " + defenseCharacter)
+alert(
+    attackingCharacter + "\nPoder de ataque: " + attackPower + "\n\n" +
+    defenseCharacter + "\nPontos de Vida: " + life +
+    "\nPoder de Defesa: " + defencePower +
+    "\nPossui Escudo: " + shield
+)
+
+
+
+    
