9fb47b50b192a699506863db791efecc8e2fb778
Lívia Calderan <liv_calderan@hotmail.com>
2023-05-24 14:16:11 -0400

    Revert "Merge https://github.com/LiviaCalderan/EstudosWeb"
    This reverts commit 2f784b508a1d0a103f65be661c129d4be02927f9, reversing
    changes made to 9b5988bfa7215b10aaf002250880afb330ec2ab4.

--- JavaScript/Exercicios/Imóveis/cadastroimóveis.js
+++ JavaScript/Exercicios/Imóveis/cadastroimóveis.js
@@ -1,60 +1,7 @@
 let opcao = ""
-let imoveisArray = []
+let imoveisCadastrados = []
+
 
 do{
-    if (imoveisArray.length > 0) {
-        opcao = prompt(
-            "Quantidade de imóveis cadastrados: " + imoveisArray.length +
-            "\n\n Insira a opção que desejar:" +
-            "\n\n1 - Salvar um imóvel" +
-            "\n2 - Ver imóveis salvos" +
-            "\n3 - Sair"
-        )
-    } else {
-        opcao = prompt(
-            "Ainda não há imóveis cadastrados" + 
-            "\n\n Insira a opção que desejar:" +
-            "\n\n1 - Salvar um imóvel" +
-            "\n2 - Ver imóveis salvos" +
-            "\n3 - Sair"
-        )    
-    }
-
-    switch(opcao){
-
-        case "1":
-            const imoveis = {}
-            imoveis.proprietario = prompt("Nome do proprietario:")
-            imoveis.quartos = prompt("Quantidade de quartos:")
-            imoveis.banheiros = prompt("Quantidade de banheiros:")
-            imoveis.garagem = prompt("Possui garagem? sim/nao")
-            imoveisArray.push(imoveis)
-
-            break
-
-        case "2":
-            if(imoveisArray.length == 0){
-                alert("Não há imoveis cadastrados!")
-            } else {
-                imoveisArray.forEach((imovel, index) => {
-                    console.log(`Imóvel ${index + 1}`);
-                    console.log(`Proprietário: ${imovel.proprietario}`);
-                    console.log(`Quartos: ${imovel.quartos}`);
-                    console.log(`Banheiros: ${imovel.banheiros}`);
-                    console.log(`Garagem: ${imovel.garagem ? "Sim" : "Não"}`);
-                    console.log("------------------------");
-                  });
-                
-            }
-            break
-
-        case "3":
-            alert("Finalizando...")
-            break
-
-        default:
-            alert("Insira uma opção válida!")
-
-    }
-
-    } while (opcao != "3")
+  
+    } while (opcao !=="3")
