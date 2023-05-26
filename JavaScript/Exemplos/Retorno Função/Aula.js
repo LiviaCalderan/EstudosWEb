0c11e5a29ac2d800f5eb673e12aff64baff822f7
Lívia Calderan <liv_calderan@hotmail.com>
2023-05-26 10:29:34 -0400

    Retorno de funções

---
+++ JavaScript/Exemplos/Retorno Função/Aula.js
@@ -0,0 +1,10 @@
+//Retorno é uma saída da função
+
+//O retorno é usado para armazenarmos a saída de uma função em uma varíalvel
+
+//Uma função sem saída explícita retornará undefined
+function calcularMedia (a, b){
+    const media = (a + b) / 2
+}
+const resultado = calcularMedia(5, 9)
+console.log(resultado)
